e55f4db45e69130287f1f91d995c8daa3c40a34b
Veli Emre Çetin <50757076+WeileeTheWeener@users.noreply.github.com>
2023-07-06 18:04:40 +0300

    (feat): added target frame rate
    -added targetframeratecomponent script

---
+++ Assets/Scripts/TargetFrameRateComponent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRateComponent : MonoBehaviour
+{
+    [SerializeField] private int targetFramerate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+
+}
